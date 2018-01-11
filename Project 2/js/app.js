ef1675dbaf33a54f1429da8ba6fbdc2ccb0af886
Prejith Premkumar <prajithpremg@gmail.com>
2018-01-11 23:50:10 +0530

    fix collision bug

--- Project 2/js/app.js
+++ Project 2/js/app.js
@@ -2,21 +2,27 @@ Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)];
 }
 
-let possibleProbabilities = [0.25, 0.30,0.60, 0.75, 0.80];
+let possibleProbabilities = [0.25, 0.30, 0.40, 0.60, 0.75, 0.80];
+let gameStarted = false;
+
+function betweenPositions(val1, val2) {
+    if (val1 > (val2 - 40) && val1 < (val2 + 40)) {
+        return true;
+    }
+}
 
 // Enemies our player must avoid
-
 class Enemy {
     constructor() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
 
-        this.possibleStarterPosns = [225,140,55];
-        this.possibleSpeeds = [3,5,6,9];
+        this.possibleStarterPosns = [225, 140, 55];
+        this.possibleSpeeds = [4, 5.25, 6];
         this.actualSpeed = this.possibleSpeeds.randomElement();
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
         this.x = -100;
         this.y = this.possibleStarterPosns.randomElement();
@@ -29,7 +35,11 @@ class Enemy {
     // all computers.
     update(dt) {
         this.x += this.actualSpeed;
-    } 
+        if ((player.y === this.y) && betweenPositions(player.x, this.x)) {
+            player.resetPlayer();
+            console.log('collide') //collision is not happening when speed is increasing at too great a value
+        }
+    }
 
     // Draw the enemy on the screen, required method for game
     render() {
@@ -42,9 +52,9 @@ class Enemy {
 // a handleInput() method.
 class Player {
     constructor() {
-      this.sprite = 'images/char-boy.png';
-      this.x = 200;
-      this.y = 305;
+        this.sprite = 'images/char-boy.png';
+        this.x = 200;
+        this.y = 310;
     }
 
     render() {
@@ -55,24 +65,33 @@ class Player {
 
     }
 
+    resetPlayer() {
+        this.x = 200;
+        this.y = 310;
+    }
+
     handleInput(keycode) {
-        switch(keycode) {
+        gameStarted = true;
+        switch (keycode) {
             case 'left':
-                if(!(this.x === 0))
+                if (!(this.x === 0))
                     this.x -= 100;
                 break;
             case 'right':
-                if(!(this.x === 400))
+                if (!(this.x === 400))
                     this.x += 100;
                 break;
             case 'up':
-                if(!(this.y < 50))
+                if (!(this.y < 50))
                     this.y -= 85;
                 break;
             case 'down':
-                if(!(this.y > 200))
+                if (!(this.y > 200))
                     this.y += 85;
                 break;
+        }
+        if(this.y === -30) {
+            this.resetPlayer();
         }
     }
 }
@@ -83,16 +102,23 @@ class Player {
 let player = new Player();
 let allEnemies = [];
 
-setInterval( function() {
+setInterval(function () {
     let probability = possibleProbabilities.randomElement();
-    if(probability > 0.50) {
+    if (probability > 0.50) {
         allEnemies.push(new Enemy());
     }
 }, 500);
 
+//TODO: Reset score on game reset
+setInterval(function () {
+    if (gameStarted) {
+        document.getElementsByClassName('score')[0].textContent++;
+    }
+}, 1000);
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
