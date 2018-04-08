56a9b04ef7ac178910da702f5bc5742849e3a72e
Prejith Premkumar <prajithpremg@gmail.com>
2018-04-08 10:10:50 +0530

    setup for foursquare api pull

--- Project 4/js/app.js
+++ Project 4/js/app.js
@@ -3,37 +3,58 @@ var locations = [{
             lat: 9.994188,
             lng: 76.358455
         },
-        title: 'Rajagiri'
+        title: 'Rajagiri',
+        visible: true,
+        fsid: ''
     },
     {
         position: {
             lat: 9.992949,
             lng: 76.357550
         },
-        title: 'Chaayakada'
+        title: 'Chaayakada',
+        visible: true,
+        fsid: ''
     },
     {
         position: {
             lat: 9.993730,
             lng: 76.346962
         },
-        title: 'Cricket ground'
+        title: 'Cricket ground',
+        visible: true,
+        fsid: ''
     },
     {
         position: {
             lat: 9.994268,
             lng: 76.357144
         },
-        title: 'South Indian Bank ATM'
+        title: 'South Indian Bank ATM',
+        visible: true,
+        fsid: ''
     },
     {
         position: {
             lat: 9.995134,
             lng: 76.360288
         },
-        title: 'Caesar\'s Cafe'
+        title: 'Caesar\'s Cafe',
+        visible: true,
+        fsid: ''
+    },
+    {
+        position: {
+            lat: 10.027805,
+            lng: 76.308762
+        },
+        title: 'Lulu mall',
+        fsid: '4c892ca2a0ffb60ce24b28c5'
     }
 ];
+
+var clientId = '1ADNIJHB33NI2I2IFRHKYFWSU3KTUEQXH3O35IVX3UXPTV1V';
+var secretId = 'CZIO5JLQYN0OHUBYPMTC3SOKZ3RUIRKMMMLLEUXC45PXUFUZ';
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -50,6 +71,11 @@ function initMap() {
 function fillInfoWindow(marker, infoWindow) {
     if (infoWindow.marker != marker) {
         infoWindow.marker = marker;
+
+        let fsUrl = "https://api.foursquare.com/v2/venues/" + 
+
+        fetch()
+
         infoWindow.setContent(`<div>${marker.title}</div>`);
         infoWindow.open(map, marker);
 
@@ -61,13 +87,14 @@ function fillInfoWindow(marker, infoWindow) {
 
 var ViewModel = function () {
     var self = this;
-    
+
     var bounds = new google.maps.LatLngBounds();
     var infoWindow = new google.maps.InfoWindow();
 
-    self.markers = [];
+    this.markers = [];
+    this.filterText = ko.observable();
 
-    locations.forEach(function(location) {
+    locations.forEach(function (location) {
         self.markers.push(new google.maps.Marker({
             position: location.position,
             title: location.title,
@@ -81,10 +108,49 @@ var ViewModel = function () {
         bounds.extend(currentMarker.position);
         currentMarker.setVisible(true);
 
-        currentMarker.addListener('click', function() {
+        currentMarker.addListener('click', function () {
             fillInfoWindow(currentMarker, infoWindow);
         });
     });
 
     map.fitBounds(bounds);
+
+    self.isSamePlace = function (place1, place2) {
+        if (place1.toLowerCase().indexOf(place2.toLowerCase()) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    self.showAllMarkers = function () {
+        self.markers.forEach(function (marker) {
+            marker.setVisible(true);
+        });
+    }
+
+    self.applyFilters = function () {
+        let inputText = self.filterText();
+        if (inputText.length !== 0) {
+            self.markers.forEach(function (marker) {
+                if (self.isSamePlace(marker.title, inputText)) {
+                    marker.setVisible(true);
+                } else {
+                    marker.setVisible(false);
+                }
+            });
+        } else {
+            self.showAllMarkers();
+        }
+        infoWindow.close();
+    };
+
+    self.setSelection = function (marker) {
+        self.filterText(marker.title);
+        self.applyFilters();
+    }
+
+    self.getFSInfo = function(marker) {
+
+    }
 }
